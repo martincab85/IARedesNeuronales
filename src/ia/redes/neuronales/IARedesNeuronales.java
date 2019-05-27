e1ba9025c80a91163e413d5054d374e737ec29a3
MartinCab <MartinCab@DESKTOP-V6DHT50>
2019-05-27 03:10:12 -0300

    cambios pero sin modularizar

--- src/ia/redes/neuronales/IARedesNeuronales.java
+++ src/ia/redes/neuronales/IARedesNeuronales.java
@@ -10,7 +10,6 @@ public class IARedesNeuronales {
     public static void main(String[] args) {
         //entrenamiento
             //perfiles
-            //int ortog;
             List<int[]> lista= new ArrayList();
             int perfilAR[]  = { 1, 1, 1, 1, 1, 1,-1,-1,-1, 1, 1, 1,-1, 1, 1, 1,-1,-1, 1};
             int perfilRR[]  = { 1,-1,-1, 1, 1,-1,-1,-1, 1,-1, 1,-1,-1,-1, 1,-1, 1, 1,-1};
@@ -24,6 +23,7 @@ public class IARedesNeuronales {
             }else{
                  System.out.println("menos de 50% de diferencia entre vectores AR y RR");System.out.println("");
             }
+            ortogonal = 0;
             for(int i=0;i<19;i++){
                 ortogonal = perfilAR[i]*perfilNR[i]+ortogonal;
             }
@@ -32,6 +32,7 @@ public class IARedesNeuronales {
             }else{
                  System.out.println("menos de 50% de diferencia entre vectores AR y NR");System.out.println("");
             }
+            ortogonal = 0;
             for(int i=0;i<19;i++){
                 ortogonal = perfilNR[i]*perfilRR[i]+ortogonal;
             }
@@ -61,9 +62,11 @@ public class IARedesNeuronales {
             //vector de entrada
             int acumulador;
             int iteracion = 0;
-            int auxEntradaPerfil [] = new int[19];
-                                   
-            int entradaPerfil[]  = {-1,-1,-1,1,-1,1,-1,-1,-1,1,-1,1,-1,1,-1,1,-1,-1,-1};
+//            int entradaPerfil[]  = { 1, 1, 1, 1, 1, 1,-1,-1,-1, 1, 1, 1,-1, 1, 1, 1,-1,-1, 1};
+//            int entradaPerfil[]  = { 1,-1,-1, 1, 1,-1,-1,-1, 1,-1, 1,-1,-1,-1, 1,-1, 1, 1,-1};
+//            int entradaPerfil[]  = {-1,-1,-1,-1,-1,-1, 1, 1, 1, 1,-1, 1, 1,-1,-1,-1,-1,-1,-1};                     
+              int entradaPerfil[]  = {-1, 1,-1, 1,-1, 1,-1,-1,-1, 1,-1, 1, 1,-1,-1,-1,-1,-1,-1};
+            ortogonal = 0;
             for(int i=0;i<19;i++){
                 ortogonal = entradaPerfil[i]*perfilRR[i]+ortogonal;
             }
@@ -72,6 +75,7 @@ public class IARedesNeuronales {
             }else{
                  System.out.println("menos de 50% de diferencia entre vectores perfilEntrada y RR");System.out.println("");
             }
+            ortogonal = 0;
             for(int i=0;i<19;i++){
                 ortogonal = perfilAR[i]*entradaPerfil[i]+ortogonal;
             }
@@ -80,6 +84,7 @@ public class IARedesNeuronales {
             }else{
                  System.out.println("menos de 50% de diferencia entre vectores AR y perfilEntrada");System.out.println("");
             }
+            ortogonal = 0;
             for(int i=0;i<19;i++){
                 ortogonal = entradaPerfil[i]*perfilNR[i]+ortogonal;
             }
@@ -96,62 +101,67 @@ public class IARedesNeuronales {
             }
             System.out.println("");System.out.println("");
             
-            
-            
-            boolean bandera = false;
             lista.add(entradaPerfil);
-            
+            boolean bandera = false;
+            boolean reconocio = false;
             while(bandera==false){
+                
+                int auxEntradaPerfil [] = new int[19];           
                 iteracion = iteracion +1;
-                System.out.println("--------iteracion-----------  "+iteracion);
+                System.out.println("-------vector-iteracion-----------  "+iteracion);
                 for(int i = 0;i<19;i++){
                     acumulador = 0;
                     for(int j = 0;j<19;j++){
                         acumulador = acumulador + entradaPerfil[j]*matrizPerfiles[i][j];
                     }
-                    if(acumulador > 0){
+                    if(acumulador >= 0){
                         auxEntradaPerfil [i] = 1;   
                     }
                     if(acumulador < 0){
                         auxEntradaPerfil [i] = -1;   
                     }
                     //mas que nada para verlo
-                    if(acumulador == 0){
-                        auxEntradaPerfil [i] = entradaPerfil [i];   
-                    }
+//                    if(acumulador == 0){
+//                        auxEntradaPerfil [i] = entradaPerfil [i];   
+//                    }
                     System.out.print(auxEntradaPerfil[i]+"\t");
                 }
                 System.out.println("");System.out.println("");
-                if(Arrays.equals(auxEntradaPerfil,entradaPerfil)==true){
-                    System.out.println("perfil aux igual a perfil entrada ");
-                    if(Arrays.equals(auxEntradaPerfil,perfilAR)==true){
+                
+                
+            
+                for(int i = 0;i < lista.size();i++){
+                    if(Arrays.equals(lista.get(i),auxEntradaPerfil)==true){
+                        bandera = true;
+                        System.out.println("encontro vector igual a alguno de la lista indice "+i);System.out.println("");
+                    }
+                }   
+                if(bandera==true){    
+                    if(Arrays.equals(entradaPerfil,perfilAR)==true){
                         System.out.println("perfil aux igual a perfil AR ");
-                        bandera = true;
-                    }
-                    if(Arrays.equals(auxEntradaPerfil,perfilRR)==true){
+                        reconocio=true;
+                    }    
+                    if(Arrays.equals(entradaPerfil,perfilRR)==true){
                         System.out.println("perfil aux igual a perfil RR  ");
-                        bandera = true;
+                        reconocio=true;
                     }
-                    if(Arrays.equals(auxEntradaPerfil,perfilNR)==true){
+                    if(Arrays.equals(entradaPerfil,perfilNR)==true){
                         System.out.println("perfil aux igual a perfil NR ");
-                        bandera = true;
+                        reconocio=true;
                     }    
                 }else{
+                    lista.add(auxEntradaPerfil);
                     System.out.println("entro a reemplazar vector ");
                     System.out.println("");
-                    System.out.println("vector aux");
-                    for(int i=0;i<19;i++){
-                        System.out.print(auxEntradaPerfil[i]+"\t");
-                    }
-                    System.out.println("");System.out.println("");
-                    System.out.println("vector entrada reemplazado por aux ");
                     for(int i=0;i<19;i++){
                         entradaPerfil[i] = auxEntradaPerfil[i];
-                        System.out.print(entradaPerfil[i]+"\t");
                     }
-                    System.out.println("");System.out.println("");
+                    
                 }
-            } 
+            }    
+            if(reconocio==false){
+                System.out.println("no se pudo asociar a un perfil ");
+            }
     }
     
 }
